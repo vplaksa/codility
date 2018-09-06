1f42992785cbd4751420f683dc7fa43393f44e8f
Vladimir Plaksa <vladimir.plaksa@mysale.com>
2018-09-06 16:36:36 +0300

    test

---
+++ Solutions/GreenRedElementsSolution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Solutions
+{
+	public class GreenRedElementsSolution
+	{
+		public int solution(int X, int[] A)
+		{
+			// write your code in C# 6.0 with .NET 4.5 (Mono)
+			//make array of indicators where +1 is green and -1 is red
+
+			int[] ind = new int[A.Length];
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (A[i] == X)
+					ind[i] = +1; //green
+				else
+					ind[i] = -1; //red;
+			}
+			//ok, lets find index for which number of +1 in left  fits number of -1 right 
+
+			int border = 0;
+			for (border = 0; border < ind.Length; border++)
+			{
+				int numberOfGreenLeft = 0;
+				int numberOfRedLeft = 0;
+
+				int numberOfGreenRight = 0;
+				int numberOfRedRight = 0;
+
+				//current left part
+				for (int i = 0; i <= border; i++) //inclusive
+				{
+					if (ind[i] >= 0)
+						numberOfGreenLeft += ind[i];
+					else
+						numberOfRedLeft += ind[i];
+				}
+
+				//current right part
+				for (int i = border + 1; i < ind.Length; i++) //inclusive
+				{
+					if (ind[i] >= 0)
+						numberOfGreenRight += ind[i];
+					else
+						numberOfRedRight += -1 * ind[i];
+				}
+
+				//check if satisfied
+				if (numberOfGreenLeft == numberOfRedRight)
+					return border + 1; //when index is three, desired num is 4
+			}
+
+			return border + 1;
+
+		}
+
+	}
+}
---
+++ Tests/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Solutions;
+
+namespace Tests
+{
+	[TestFixture]
+	public class TestClass
+	{
+		[Test]
+		public void TestGreedRedDigits()
+		{
+			var s = new GreenRedElementsSolution();
+
+			Assert.AreEqual(4, s.solution(5, new int[] {5, 5, 1, 7, 2, 3, 5}));
+			Assert.AreEqual(4, s.solution(5, new int[] {5, 5, Int32.MinValue, Int32.MaxValue, 2, Int32.MaxValue, 5}));
+			Assert.AreEqual(6, s.solution(7, new[] {6, 5, 4, 3, 2, 1}));
+			Assert.AreEqual(5, s.solution(5, new int[] {-5, 5, 4, -4321, 2, 1}));
+			Assert.AreEqual(5, s.solution(5, new int[] {-5, 5, 4, Int32.MinValue, Int32.MaxValue, 1}));
+		}
+	}
+}
